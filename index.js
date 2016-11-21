12e74e47fbf72f7736d5a265c04fa1456bc5cc3a
Titus Wormer <tituswormer@gmail.com>
2016-11-21 18:09:28 +0100

    Update code-style

--- index.js
+++ index.js
@@ -1,65 +1,38 @@
-/**
- * @author Titus Wormer
- * @copyright 2015 Titus Wormer
- * @license MIT
- * @module unist:util:find-after
- * @fileoverview Utility to find a node after another node.
- */
-
 'use strict';
-
-/* eslint-env commonjs */
-
-/*
- * Dependencies.
- */
 
 var is = require('unist-util-is');
 
-/**
- * Find a node after `index` in `parent` which passes
- * `test`.
- *
- * @param {Node} parent - Parent to search in.
- * @param {number|Node} index - (Position of) node to
- *   search after.
- * @param {*} test - See `wooorm/unist-util-is`.
- * @return {Node?} - A child node of `parent` which passes
- *   `test`.
- */
-function findAfter(parent, index, test) {
-    var children;
-    var child;
-    var length;
-
-    if (!parent || !parent.type || !parent.children) {
-        throw new Error('Expected parent node');
-    }
-
-    children = parent.children;
-    length = children.length;
-
-    if (index && index.type) {
-        index = children.indexOf(index);
-    }
-
-    if (isNaN(index) || index < 0 || index === Infinity) {
-        throw new Error('Expected positive finite index or child node');
-    }
-
-    while (++index < length) {
-        child = children[index];
-
-        if (is(test, child, index, parent)) {
-            return child;
-        }
-    }
-
-    return null;
-}
-
-/*
- * Expose.
- */
-
 module.exports = findAfter;
+
+/* Find a node after `index` in `parent` which passes
+ * `test`. */
+function findAfter(parent, index, test) {
+  var children;
+  var child;
+  var length;
+
+  if (!parent || !parent.type || !parent.children) {
+    throw new Error('Expected parent node');
+  }
+
+  children = parent.children;
+  length = children.length;
+
+  if (index && index.type) {
+    index = children.indexOf(index);
+  }
+
+  if (isNaN(index) || index < 0 || index === Infinity) {
+    throw new Error('Expected positive finite index or child node');
+  }
+
+  while (++index < length) {
+    child = children[index];
+
+    if (is(test, child, index, parent)) {
+      return child;
+    }
+  }
+
+  return null;
+}
--- test.js
+++ test.js
@@ -1,136 +1,126 @@
-/**
- * @author Titus Wormer
- * @copyright 2015 Titus Wormer
- * @license MIT
- * @module unist:util:find-after
- * @fileoverview Test suite for `unit-util-find-after`.
- */
-
 'use strict';
 
-/* eslint-env node, mocha */
-
-/*
- * Dependencies.
- */
-
 var assert = require('assert');
+var test = require('tape');
 var mdast = require('mdast');
 var findAfter = require('./');
 
-/*
- * Methods.
- */
-
-var equal = assert.strictEqual;
-
-/*
- * Fixture.
- */
-
-var ast = mdast.parse('Some *emphasis*, **strongness**, and `code`.');
-var paragraph = ast.children[0];
+var tree = mdast().parse('Some *emphasis*, **strongness**, and `code`.');
+var paragraph = tree.children[0];
 var children = paragraph.children;
 
-/*
- * Tests.
- */
+test('unist-util-find-after', function (t) {
+  t.throws(
+    function () {
+      findAfter();
+    },
+    /Expected parent node/,
+    'should fail without parent'
+  );
 
-describe('unist-util-find-after', function () {
-    it('should fail without parent', function () {
-        assert.throws(function () {
-            findAfter();
-        }, /Expected parent node/);
-    });
+  t.throws(
+    function () {
+      findAfter({type: 'foo'});
+    },
+    /Expected parent node/,
+    'should fail without parent node'
+  );
 
-    it('should fail without parent node', function () {
-        assert.throws(function () {
-            findAfter({
-                'type': 'foo'
-            });
-        }, /Expected parent node/);
-    });
+  t.doesNotThrow(
+    function () {
+      assert.throws(
+        function () {
+          findAfter({type: 'foo', children: []});
+        },
+        /Expected positive finite index or child node/
+      );
 
-    it('should fail without index', function () {
-        assert.throws(function () {
-            findAfter({
-                'type': 'foo',
-                'children': []
-            });
-        }, /Expected positive finite index or child node/);
+      assert.throws(
+        function () {
+          findAfter({type: 'foo', children: []}, -1);
+        },
+        /Expected positive finite index or child node/
+      );
 
-        assert.throws(function () {
-            findAfter({
-                'type': 'foo',
-                'children': []
-            }, -1);
-        }, /Expected positive finite index or child node/);
+      assert.throws(
+        function () {
+          findAfter({type: 'foo', children: []}, {type: 'bar'});
+        },
+        /Expected positive finite index or child node/
+      );
+    },
+    'should fail without index'
+  );
 
-        assert.throws(function () {
-            findAfter({
-                'type': 'foo',
-                'children': []
-            }, {
-                'type': 'bar'
-            });
-        }, /Expected positive finite index or child node/);
-    });
+  t.doesNotThrow(
+    function () {
+      assert.throws(
+        function () {
+          findAfter({
+            type: 'foo',
+            children: [{type: 'bar'}, {type: 'baz'}]
+          }, 0, false);
+        },
+        /Expected function, string, or node as test/
+      );
 
-    it('should fail for invalid `test`', function () {
-        assert.throws(function () {
-            findAfter({
-                'type': 'foo',
-                'children': [{
-                    'type': 'bar'
-                }, {
-                    'type': 'baz'
-                }]
-            }, 0, false);
-        }, /Expected function, string, or node as test/);
+      assert.throws(
+        function () {
+          findAfter({
+            type: 'foo',
+            children: [{type: 'bar'}, {type: 'baz'}]
+          }, 0, true);
+        },
+        /Expected function, string, or node as test/
+      );
+    },
+    'should fail for invalid `test`'
+  );
 
-        assert.throws(function () {
-            findAfter({
-                'type': 'foo',
-                'children': [{
-                    'type': 'bar'
-                }, {
-                    'type': 'baz'
-                }]
-            }, 0, true);
-        }, /Expected function, string, or node as test/);
-    });
+  t.doesNotThrow(
+    function () {
+      assert.strictEqual(findAfter(paragraph, children[1]), children[2]);
+      assert.strictEqual(findAfter(paragraph, 1), children[2]);
+      assert.strictEqual(findAfter(paragraph, 7), null);
+    },
+    'should return the following node when without `test`'
+  );
 
-    it('should return the following node when without `test`', function () {
-        equal(findAfter(paragraph, children[1]), children[2]);
-        equal(findAfter(paragraph, 1), children[2]);
-        equal(findAfter(paragraph, 7), null);
-    });
+  t.doesNotThrow(
+    function () {
+      assert.strictEqual(findAfter(paragraph, 0, children[6]), children[6]);
+      assert.strictEqual(findAfter(paragraph, children[0], children[1]), children[1]);
+      assert.strictEqual(findAfter(paragraph, 0, children[1]), children[1]);
+      assert.strictEqual(findAfter(paragraph, children[0], children[0]), null);
+      assert.strictEqual(findAfter(paragraph, 0, children[0]), null);
+      assert.strictEqual(findAfter(paragraph, 1, children[1]), null);
+    },
+    'should return `node` when given a `node` and existing'
+  );
 
-    it('should return `node` when given a `node` and existing', function () {
-        equal(findAfter(paragraph, 0, children[6]), children[6]);
-        equal(findAfter(paragraph, children[0], children[1]), children[1]);
-        equal(findAfter(paragraph, 0, children[1]), children[1]);
-        equal(findAfter(paragraph, children[0], children[0]), null);
-        equal(findAfter(paragraph, 0, children[0]), null);
-        equal(findAfter(paragraph, 1, children[1]), null);
-    });
+  t.doesNotThrow(
+    function () {
+      assert.strictEqual(findAfter(paragraph, 0, 'strong'), children[3]);
+      assert.strictEqual(findAfter(paragraph, 3, 'strong'), null);
+      assert.strictEqual(findAfter(paragraph, children[0], 'strong'), children[3]);
+      assert.strictEqual(findAfter(paragraph, children[3], 'strong'), null);
+    },
+    'should return a child when given a `type` and existing'
+  );
 
-    it('should return a child when given a `type` and existing', function () {
-        equal(findAfter(paragraph, 0, 'strong'), children[3]);
-        equal(findAfter(paragraph, 3, 'strong'), null);
-        equal(findAfter(paragraph, children[0], 'strong'), children[3]);
-        equal(findAfter(paragraph, children[3], 'strong'), null);
-    });
+  t.doesNotThrow(
+    function () {
+      assert.strictEqual(findAfter(paragraph, 0, test), children[5]);
+      assert.strictEqual(findAfter(paragraph, 5, test), null);
+      assert.strictEqual(findAfter(paragraph, children[4], test), children[5]);
+      assert.strictEqual(findAfter(paragraph, children[6], test), null);
 
-    it('should return a child when given a `test` and existing', function () {
-        /** Test */
-        function test(node, n) {
-            return n === 5;
-        }
+      function test(node, n) {
+        return n === 5;
+      }
+    },
+    'should return a child when given a `test` and existing'
+  );
 
-        equal(findAfter(paragraph, 0, test), children[5]);
-        equal(findAfter(paragraph, 5, test), null);
-        equal(findAfter(paragraph, children[4], test), children[5]);
-        equal(findAfter(paragraph, children[6], test), null);
-    });
+  t.end();
 });
