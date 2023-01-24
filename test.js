46dc0697bef14d97c9c8dd25b4f8bac49dc8cfbd
Titus Wormer <tituswormer@gmail.com>
2023-01-24 17:24:41 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -3,7 +3,8 @@
  * @typedef {import('unist').Parent} Parent
  */
 
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {fromMarkdown} from 'mdast-util-from-markdown'
 import {findAfter} from './index.js'
 
@@ -11,8 +12,8 @@ const tree = fromMarkdown('Some _emphasis_, **importance**, and `code`.')
 const paragraph = /** @type {Parent} */ (tree.children[0])
 const children = paragraph.children
 
-test('unist-util-find-after', (t) => {
-  t.throws(
+test('unist-util-find-after', () => {
+  assert.throws(
     () => {
       // @ts-expect-error runtime.
       findAfter()
@@ -21,7 +22,7 @@ test('unist-util-find-after', (t) => {
     'should fail without parent'
   )
 
-  t.throws(
+  assert.throws(
     () => {
       // @ts-expect-error runtime.
       findAfter({type: 'foo'})
@@ -30,7 +31,7 @@ test('unist-util-find-after', (t) => {
     'should fail without parent node'
   )
 
-  t.throws(
+  assert.throws(
     () => {
       // @ts-expect-error runtime.
       findAfter({type: 'foo', children: []})
@@ -39,7 +40,7 @@ test('unist-util-find-after', (t) => {
     'should fail without index (#1)'
   )
 
-  t.throws(
+  assert.throws(
     () => {
       findAfter({type: 'foo', children: []}, -1)
     },
@@ -47,7 +48,7 @@ test('unist-util-find-after', (t) => {
     'should fail without index (#2)'
   )
 
-  t.throws(
+  assert.throws(
     () => {
       findAfter({type: 'foo', children: []}, {type: 'bar'})
     },
@@ -55,7 +56,7 @@ test('unist-util-find-after', (t) => {
     'should fail without index (#3)'
   )
 
-  t.throws(
+  assert.throws(
     () => {
       findAfter(
         {type: 'foo', children: [{type: 'bar'}, {type: 'baz'}]},
@@ -68,7 +69,7 @@ test('unist-util-find-after', (t) => {
     'should fail for invalid `test` (#1)'
   )
 
-  t.throws(
+  assert.throws(
     () => {
       findAfter(
         {type: 'foo', children: [{type: 'bar'}, {type: 'baz'}]},
@@ -81,102 +82,100 @@ test('unist-util-find-after', (t) => {
     'should fail for invalid `test` (#2)'
   )
 
-  t.strictEqual(
+  assert.strictEqual(
     findAfter(paragraph, children[1]),
     children[2],
     'should return the following node when without `test` (#1)'
   )
-  t.strictEqual(
+  assert.strictEqual(
     findAfter(paragraph, 1),
     children[2],
     'should return the following node when without `test` (#1)'
   )
-  t.strictEqual(
+  assert.strictEqual(
     findAfter(paragraph, 7),
     null,
     'should return the following node when without `test` (#1)'
   )
 
-  t.strictEqual(
+  assert.strictEqual(
     findAfter(paragraph, 0, children[6]),
     children[6],
     'should return `node` when given a `node` and existing (#1)'
   )
-  t.strictEqual(
+  assert.strictEqual(
     findAfter(paragraph, children[0], children[1]),
     children[1],
     'should return `node` when given a `node` and existing (#2)'
   )
-  t.strictEqual(
+  assert.strictEqual(
     findAfter(paragraph, 0, children[1]),
     children[1],
     'should return `node` when given a `node` and existing (#3)'
   )
-  t.strictEqual(
+  assert.strictEqual(
     findAfter(paragraph, children[0], children[0]),
     null,
     'should return `node` when given a `node` and existing (#4)'
   )
-  t.strictEqual(
+  assert.strictEqual(
     findAfter(paragraph, 0, children[0]),
     null,
     'should return `node` when given a `node` and existing (#5)'
   )
-  t.strictEqual(
+  assert.strictEqual(
     findAfter(paragraph, 1, children[1]),
     null,
     'should return `node` when given a `node` and existing (#6)'
   )
 
-  t.strictEqual(
+  assert.strictEqual(
     findAfter(paragraph, 0, 'strong'),
     children[3],
     'should return a child when given a `type` and existing (#1)'
   )
-  t.strictEqual(
+  assert.strictEqual(
     findAfter(paragraph, 3, 'strong'),
     null,
     'should return a child when given a `type` and existing (#2)'
   )
-  t.strictEqual(
+  assert.strictEqual(
     findAfter(paragraph, children[0], 'strong'),
     children[3],
     'should return a child when given a `type` and existing (#3)'
   )
-  t.strictEqual(
+  assert.strictEqual(
     findAfter(paragraph, children[3], 'strong'),
     null,
     'should return a child when given a `type` and existing (#4)'
   )
 
-  t.strictEqual(
-    findAfter(paragraph, 0, test),
+  assert.strictEqual(
+    findAfter(paragraph, 0, check),
     children[5],
     'should return a child when given a `test` and existing (#1)'
   )
-  t.strictEqual(
-    findAfter(paragraph, 5, test),
+  assert.strictEqual(
+    findAfter(paragraph, 5, check),
     null,
     'should return a child when given a `test` and existing (#2)'
   )
-  t.strictEqual(
-    findAfter(paragraph, children[4], test),
+  assert.strictEqual(
+    findAfter(paragraph, children[4], check),
     children[5],
     'should return a child when given a `test` and existing (#3)'
   )
-  t.strictEqual(
-    findAfter(paragraph, children[6], test),
+  assert.strictEqual(
+    findAfter(paragraph, children[6], check),
     null,
     'should return a child when given a `test` and existing (#4)'
   )
 
   /**
    * @param {Node} _
-   * @param {number} n
+   * @param {number | null | undefined} n
    */
-  function test(_, n) {
+  function check(_, n) {
     return n === 5
   }
-
-  t.end()
 })
